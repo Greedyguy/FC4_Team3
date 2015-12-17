827f19f66c7b060313bd514c7112c1d841c6c305
junkwhinger <junsik.whang@gmail.com>
2015-12-17 15:10:52 +0900

    회원가입 기능 추가
    Views에서 회원가입하는 기능
    Template_dir에 회원가입 페이지 및 완료후 페이지
    urls에서 연결하는 작업 완료

--- team_prj/profiles/views.py
+++ team_prj/profiles/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        userform = UserCreationForm(request.POST)
+        if userform.is_valid():
+            userform.save()
+
+            return HttpResponseRedirect(
+                reverse("signup_ok")
+            )
+
+    elif request.method == "GET":
+        userform = UserCreationForm()
+
+    return render(request, "signup.html", {
+        "userform": userform,
+    })
